01f9716e5e929cff07b519c29b596a0930489cf2
Morov78 <98202937+Morov78@users.noreply.github.com>
2022-08-04 10:17:37 +0300

    news

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,73 @@
+const BASE_URL = 'http://localhost:3000';
+// DELETE
 
+// function deleteBookById(bookId) {
+//   const options = {
+//     method: 'DELETE',
+//   };
+//   return fetch(`${BASE_URL}/books/${bookId}`, options)
+//     .then(res => res.json())
+//     .then(console.log());
+// }
+// deleteBookById(15);
+// deleteBookById(14);
+
+// PATCH or PUT ************ PATCH змінює передані властивості,
+// PUT повністю замінює елемент новим
+// const book = {
+//   title: 'Тестова книжка Мацюк Н.',
+// };
+// function updateBookById(update, bookId) {
+//   const options = {
+//     method: 'PATCH',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(update),
+//   };
+//   return fetch(`${BASE_URL}/books/${bookId}`, options)
+//     .then(res => res.json())
+//     .then(console.log());
+// }
+
+// updateBookById(book, 14);
+// updateBookById({ rating: 10 }, 14);
+// updateBookById({ rating: 7.7, title: 'Тестова книжка М.Н.' }, 15);
+// читалка GET  ********************************
+
+// function fetchBooks(arguments) {
+//   return fetch(`${BASE_URL}/books`)
+//     .then(res => res.json)
+//     .then(console.log);
+// }
+
+// function fetchBookById(bookId) {
+//   fetch(`${BASE_URL}/${bookId}`)
+//     .then(res => res.json)
+//     .then(console.log);
+// }
+// fetchBooks();
+// fetchBookById(2);
+// писалка POST  *******
+
+// function addBook(book) {
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(book),
+//   };
+//   return fetch(`${BASE_URL}/books`, options).then(res => res.json());
+// }
+// addBook({
+//   title: 'Тестова книжка Ніка',
+//   author: 'Мацюк Ніка',
+//   genres: ['казки'],
+//   rating: 9.8,
+// }).then(renderBook).catch(error => console.log(error))
+
+// function renderBook(book) {
+//   console.log(book);
+//   console.log('tatata');
+// }
--- src/js/news.js
+++ src/js/news.js
@@ -1,27 +1,71 @@
 const KEY = '834e5c777b504c48a337b01b262c10d5';
+let timer = null;
 const params = new URLSearchParams({
   apiKey: KEY,
+  category: 'general',
   country: 'ua',
   page: 1,
   pageSize: 10,
 });
+
 const refs = {
+  outputNumberResults: document.querySelector('.js-output'),
+  outputCurrentResults: document.querySelector('.js-output-item'),
   outputTotalResults: document.querySelector('.js-output-total'),
   outputArticles: document.querySelector('.js-articles'),
+  form: document.querySelector('.form'),
+  inputSelect: document.querySelector('.form select'),
+  queryButton: document.querySelector('.form button'),
 };
-fetch(`https://newsapi.org/v2/top-headlines?${params}`).then(response =>
-  response.json().then(data => fetchData(data))
-);
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  refs.outputArticles.innerHTML = '';
+  params.set('category', refs.inputSelect.value);
+  params.set('page', 1);
+  refs.outputNumberResults.classList.remove('is-hidden');
+  findFetch(params);
+  timer = setInterval(infiniteScroll, 300);
+}
+// функція безперервного скрола
+function infiniteScroll() {
+  const heightWindow = window.innerHeight;
+  const positionY = window.scrollY;
+  const maxY = refs.outputArticles.clientHeight;
+  if ((maxY - positionY < 1.7 * heightWindow) & (positionY > heightWindow)) {
+    params.set('page', Number(params.get('page')) + 1);
+    // console.log(params.toString());
+
+    findFetch(params);
+  }
+}
+
+function findFetch(params) {
+  fetch(`https://newsapi.org/v2/top-headlines?${params}`).then(response =>
+    response.json().then(data => fetchData(data))
+  );
+}
 
 function fetchData(items) {
-  outputTotalResults(items.totalResults);
-  console.log(items.articles);
-  renderArticles(items.articles);
-}
+  currentItems =
+    items.articles.length + (params.get('page') - 1) * params.get('pageSize');
+  outputTotalResults(currentItems, items.totalResults);
 
-function outputTotalResults(numberResults) {
-  refs.outputTotalResults.textContent = numberResults;
+  renderArticles(items.articles);
+  const isInterval = currentItems >= items.totalResults;
+
+  // console.log(isInterval);
+  if (isInterval) {
+    clearInterval(timer);
+  }
 }
+// виводить поточну кількість результатів і максимальну кількість результатів
+function outputTotalResults(currentResults, numberResults) {
+  refs.outputCurrentResults.textContent = currentResults;
+  numberResults ? (refs.outputTotalResults.textContent = numberResults) : null;
+}
+// рендер новин на сторінку
 function renderArticles(articles) {
   const makeArticles = articles.reduce(
     (acc, article) => acc + makeArticle(article),
@@ -29,7 +73,7 @@ function renderArticles(articles) {
   );
   refs.outputArticles.insertAdjacentHTML('beforeend', makeArticles);
 }
-
+// створює розмітку однієї новини
 function makeArticle({
   title,
   author,
@@ -41,7 +85,9 @@ function makeArticle({
   return `
   <li class="list__item">
     <a class="list__link" href="${url}" target="_blank" rel="noopener noreferrer">
-      <img src="${urlToImage}" alt="${description}">
+      ${
+        urlToImage ? `<img src="${urlToImage}" alt="${description}"></img>` : ''
+      }
       <h2>${title}</h2>
       <p><b>Author</b>: ${author}</p>
       <p><b>Published Date</b>: ${publishedAt}</p>
@@ -49,59 +95,3 @@ function makeArticle({
     </a>
   </li>`;
 }
-//// author: "Українська правда"
-//// content: ", .\r\n: \r\n: \" , . . , .\r\n , , . . , , – .\r\n , \".\r\n: , .\r\n\" ( – .), , . , \"HIMARS\", \", – .\r\n , , , .\r\n\" , , . , \", - .\r\n: 31 Politico Maxar, .\r\n InformNapalm ,   .\r\n , \" \".\r\n:\r\n<ul><li>29    , . , 53 .… [+86 chars]"
-//// description: ""
-//// publishedAt: "2022-08-01T18:04:27Z"
-// source: {id: null, name: "Pravda.com.ua"}
-//// title: "Полонених в Оленівці могли вбити за допомогою термобаричної зброї – генпрокурор - Українська правда"
-// url: "https://www.pravda.com.ua/news/2022/08/1/7361416/"
-//// urlToImage: "https://img.pravda.com/images/doc/9/0/9055996-olenivka-suputnyk.jpg"
-// const fetchUsers = async () => {
-//   const baseUrl = "https://jsonplaceholder.typicode.com";
-//   const userIds = [1, 2, 3];
-
-//   // 1. Створюємо масив промісів
-//   const arrayOfPromises = userIds.map(async userId => {
-//     const response = await fetch(`${baseUrl}/users/${userId}`);
-//     return response.json();
-//   });
-
-//   // 2. Запускаємо усі проміси паралельно і чекаємо на їх завершення
-//   const users = await Promise.all(arrayOfPromises);
-//   console.log(users);
-// };
-
-// fetchUsers();
-
-// const YOUR_API_KEY = '8b0c7f7b-6f51-46e6-aecc-c1399eedc5c7';
-// const BASE_URL = 'http://eventregistry.org/api/v1/article';
-// const options = {action: "getArticles",
-//     // "keyword": "Barack Obama",
-//     // "articlesPage": 1,
-//     // "articlesCount": 100,
-//     // "articlesSortBy": "date",
-//     // "articlesSortByAsc": false,
-//     // "articlesArticleBodyLen": -1,
-//     // "resultType": "articles",
-//     // "dataType": [
-//     //   "news",
-//     //   "pr"
-//     // ],
-//     apiKey: "8b0c7f7b-6f51-46e6-aecc-c1399eedc5c7",
-//     // "forceMaxDataTimeWindow": 31,
-//   };
-//   const po=JSON.stringify(options);
-// console.log(po);
-
-// fetch(`http://eventregistry.org/api/v1/article/getArticles?`,po).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     console.log(response.status);
-//     return response.json();
-// }).then(data => console.log(data))
-//     .catch(error=>console.log(error))
-// console.log();
-// console.log('');
-// console.log();
